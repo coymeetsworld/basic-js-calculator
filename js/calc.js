8de1e71e707e1954ee0b442496370a6d7b0b95f6
Coy Sanders <coymeetsworld@gmail.com>
2017-03-25 00:15:55 -0700

    More refactoring

--- js/calc.js
+++ js/calc.js
@@ -1,221 +1,192 @@
-$(document).ready(function() {
+$(document).ready(() => {
 
+  const MAX_NUMBER_SIZE = 8;
+  const OP_MULTIPLY_SYMBOL = "*";
+  const OP_ADD_SYMBOL = "+";
+  const OP_MINUS_SYMBOL = "-";
+  const OP_DIVIDE_SYMBOL = "/";
 
   $("#screen").append("<p id=\"screen-val\">0</p>");
 
-  var operators = ["+", "-", "*", "/"];
-  var equation_parts = [];
+  let operators = [OP_ADD_SYMBOL, OP_MINUS_SYMBOL, OP_MULTIPLY_SYMBOL, OP_DIVIDE_SYMBOL];
+  let equationParts = [];
 
-  var current_val = "0";
-  var current_operator = null;
+  let currVal = 0;
+  let currOperator = null;
 
-  function clearScreen() {
+  const clearScreen = () => {
     $("#screen-val").html(0);
   }
 
   /* Updates the current value on the screen. If a operator key was pressed previous to this, that key and the value on the screen prior are stored in the array for future processing. */
-  function processNumClick(num_id) {
+  const processNumClick = (numID) => {
 
-    if (current_operator) {
-      console.log("Pushing " + current_val + " to the queue");
-      equation_parts.push(current_val);
-      current_val = "0";
-      console.log("Pushing " + current_operator + " to the queue");
-      equation_parts.push(current_operator);
-      current_operator = null;
+    if (currOperator) {
+      equationParts.push(currVal);
+      currVal = 0;
+      equationParts.push(currOperator);
+      currOperator = null;
     }
 
-    /* Screen only takes up to 8-digit numbers */
-    if (current_val.toString().length == 8) {
+    if (currVal.toString().length === MAX_NUMBER_SIZE) {
       return;
     }
-
-    var num;
-    switch(num_id) {
+    
+    let num;
+    switch(numID) {
       case "key-1":
-         num = 1;
-         break;
+        num = 1;
+        break;
       case "key-2":
-         num = 2;
-         break;
+        num = 2;
+        break;
       case "key-3":
-         num = 3;
-         break;
+        num = 3;
+        break;
       case "key-4":
-         num = 4;
-         break;
+        num = 4;
+        break;
       case "key-5":
-         num = 5;
-         break;
+        num = 5;
+        break;
       case "key-6":
-         num = 6;
-         break;
+        num = 6;
+        break;
       case "key-7":
-         num = 7;
-         break;
+        num = 7;
+        break;
       case "key-8":
-         num = 8;
-         break;
+        num = 8;
+        break;
       case "key-9":
-         num = 9;
-         break;
+        num = 9;
+        break;
       case "key-0":
-         num = 0;
-         break;
+        num = 0;
+        break;
       case "key-decimal":
-         num = ".";
-         break;
+        num = ".";
+        break;
       default:
-         console.log("Error processing number press.");
-    } /* End of switch statement */
-
-    if (current_val == "0") {
-      current_val = num.toString();
-    } else {
-      current_val = current_val.concat(num.toString());
+        console.log("Error processing number press.");
     }
 
-    console.log("current-val: " + current_val);
-    $("#screen-val").html(current_val);
+    if (currVal === 0) {
+      currVal = num.toString();
+    } else {
+      currVal = currVal.toString().concat(num.toString());
+    }
+
+    $("#screen-val").html(currVal);
   }
 
 
   /* Determines which operation to put in the array given which operation key was pressed. */
-  function processOperationClick(op_id) {
+  const processOperationClick = (op_id) => {
 
-    console.log("op-id: " + op_id);
-    var op;
+    let op;
     switch(op_id) {
       case "key-plus":
-         op = "+";
-         break;
+        op = OP_ADD_SYMBOL;
+        break;
       case "key-minus":
-         op = "-";
-         break;
+        op = OP_MINUS_SYMBOL;
+        break;
       case "key-times":
-         op = "*";
-         break;
+        op = OP_MULTIPLY_SYMBOL;
+        break;
       case "key-divide":
-         op = "/";
-         break;
+        op = OP_DIVIDE_SYMBOL;
+        break;
       case "key-percent":
-         generatePercentValue();
-         break;
+        generatePercentValue();
+        break;
       case "key-equals":
-         calculateValue();
-         break;
+        calculateValue();
+        break;
       default:
         console.log("Error processing operator press.");
     }
 
-    current_operator = op;
+    currOperator = op;
   }
 
 
   /* Truncates the number to something printable up to 8 decimals. */
-  function getPrintedValue(value) {
-    console.log("Calling getPrintedValue(" + value + ")");
-    return Number(Math.round(value*1000000)/1000000);
-  }
-
+  const getPrintedValue = (value) => Number(Math.round(value*1000000)/1000000);
 
   /* Divides current value on screen by 100. */
-  function generatePercentValue() {
-    console.log("Generate percent value: " );
-    current_val /= 100;
-    current_val = getPrintedValue(current_val);
-    $("#screen-val").html(current_val);
+  const generatePercentValue = () => {
+    currVal /= 100;
+    currVal = getPrintedValue(currVal);
+    $("#screen-val").html(currVal);
   }
-
-  /*
-    Helper function, just used for troubleshooting.
-  */
-  function printArray(arr) {
-    console.log("Array start: " );
-    for (var i = 0; i < arr.length; i++) {
-      console.log(arr[i]);
-    }
-    console.log("Array end.");
-  }
-
 
   /* Calculates the equation from beginning to when the equals key was pressed. */
-  function calculateValue() {
+  const calculateValue = () => {
 
-    equation_parts.push(current_val);
-    current_operator = null; /* Marking it null for next equation. */
+    equationParts.push(currVal);
+    currOperator = null; /* Marking it null for next equation. */
 
-    var value;
+    let value;
 
     /* Perform order of operations. First handle multiply and divide from left to right*/
-    for (var i = 0, items = equation_parts.length; i < items; i++) {
-        printArray(equation_parts);
-        if (equation_parts[i] == "*") {
-          value = (parseFloat(equation_parts[i-1]) * parseFloat(equation_parts[i+1]));
-          equation_parts.splice(i - 1, 3);
-          equation_parts.splice(i - 1, 0, value.toString());
-          i--;
-        }
-        if (equation_parts[i] == "/") {
-          value = (parseFloat(equation_parts[i-1]) / parseFloat(equation_parts[i+1]));
-          equation_parts.splice(i - 1, 3);
-          equation_parts.splice(i - 1, 0, value.toString());
-          i--;
-        }
+    for (let i = 0, items = equationParts.length; i < items; i++) {
+      if (equationParts[i] === OP_MULTIPLY_SYMBOL) {
+        value = (parseFloat(equationParts[i-1]) * parseFloat(equationParts[i+1]));
+        equationParts.splice(i - 1, 3);
+        equationParts.splice(i - 1, 0, value.toString());
+        i--;
+      }
+      if (equationParts[i] === OP_DIVIDE_SYMBOL) {
+        value = (parseFloat(equationParts[i-1]) / parseFloat(equationParts[i+1]));
+        equationParts.splice(i - 1, 3);
+        equationParts.splice(i - 1, 0, value.toString());
+        i--;
+      }
     }
 
     /* Now process addition/subtraction, from left to right. */
-    for (var i = 0, items = equation_parts.length; i < items; i++) {
-        printArray(equation_parts);
-        if (equation_parts[i] == "+") {
-          value = (parseFloat(equation_parts[i-1]) + parseFloat(equation_parts[i+1]));
-          equation_parts.splice(i - 1, 3);
-          equation_parts.splice(i - 1, 0, value.toString());
-          i--;
-        }
-        if (equation_parts[i] == "-") {
-          value = (parseFloat(equation_parts[i-1]) - parseFloat(equation_parts[i+1]));
-          equation_parts.splice(i - 1, 3);
-          equation_parts.splice(i - 1, 0, value.toString());
-          i--;
-        }
+    for (let i = 0, items = equationParts.length; i < items; i++) {
+      if (equationParts[i] === OP_ADD_SYMBOL) {
+        value = (parseFloat(equationParts[i-1]) + parseFloat(equationParts[i+1]));
+        equationParts.splice(i - 1, 3);
+        equationParts.splice(i - 1, 0, value.toString());
+        i--;
+      }
+      if (equationParts[i] === OP_MINUS_SYMBOL) {
+        value = (parseFloat(equationParts[i-1]) - parseFloat(equationParts[i+1]));
+        equationParts.splice(i - 1, 3);
+        equationParts.splice(i - 1, 0, value.toString());
+        i--;
+      }
     }
 
-    current_val = equation_parts[0];
-    console.log("value (not printed): " + current_val);
-    current_val = getPrintedValue(current_val);
-    $("#screen-val").html(current_val);
-    equation_parts = [];
+    currVal = getPrintedValue(equationParts[0]);
+    $("#screen-val").html(currVal);
+    equationParts = [];
   };
-
 
   /* Handles clicking one of the number keys (0-9). */
   $(".num-key").click(function() {
-    console.log("Pressed a number key.");
-    console.log("Pressed button " + this.id);
     processNumClick(this.id);
   });
 
-
   /* Handles clicking an operation key (+,-,*,/,%) */
   $(".operator-key").click(function() {
-    console.log("Pressed an operator key.");
-    console.log("Pressed button " + this.id);
     processOperationClick(this.id);
   });
 
-
   /* Handles clicking the CE button. Leaves memory in tact but clears out current value on the screen. */
-  $("#key-ce").click(function() {
-    current_val = 0;
+  $("#key-ce").click(() => {
+    currVal = 0;
     clearScreen();
   });
 
-
   /* Handles clicking the AC button. Clears out memory and the screen. */
-  $("#key-ac").click(function() {
-    current_val = 0;
+  $("#key-ac").click(() => {
+    currVal = 0;
     clearScreen();
-    equation_parts = [];
+    equationParts = [];
   });
 });
